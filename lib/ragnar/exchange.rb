b598819fda2b4230389d125ab6c802c09c7b4dfe
Tracey Eubanks <traceyeubanks@yahoo.com>
2011-08-02 13:59:09 -0600

    Revert "fixed publishing. subscribing works"
    This reverts commit bf159b7849ef8dd733b2a2b4f46dd8edfd023d54.
    
    We'll rely on what BJ wrote. As long as the exchange and queue have been
    created and bound together, Ragnar will work as it stands. Otherwise,
    things won't work as expected.

--- lib/ragnar/exchange.rb
+++ lib/ragnar/exchange.rb
@@ -1,24 +1,23 @@
 module Ragnar
   class Exchange
-
+    
     attr_reader :exchange, :channel, :type, :name, :options
     attr_accessor :queue_prefix
-
+    
     def initialize type, name, opts={}
       @type, @name, @options = type, name, opts
       @channel = AMQP::Channel.new(Ragnar::Connector.connection)
-      @exchange = @channel.__send__(@type, @name)
+      @exchange = @channel.__send__(@type, @name, @options)
     end
-
+    
     def publish routing_key, message, opts={}
-      @channel.queue(@name).bind(@exchange, opts.merge(routing_key: routing_key))
-      @exchange.publish(message, routing_key: routing_key)
+      @exchange.publish(message, opts.merge(:routing_key => routing_key))
     end
-
+    
     # Takes a subscription key or queue/routing options
     #
     #   exchange.subscribe('the.key') # => queue name and routing key are 'the.key'
-    #   exchange.subscribe(:queue => 'my.queue', :routing_key => 'message.*.pattern')
+    #   exchange.subscribe(:queue => 'my.queue', :routing_key => 'message.*.pattern') 
     #
     def subscribe name, subscribe_opts={}, &block
       if name.is_a?(Hash)
@@ -31,6 +30,6 @@ module Ragnar
       end
       @channel.queue(queue_name).bind(@exchange, :routing_key => routing_key).subscribe(subscribe_opts, &block)
     end
-
+    
   end
 end
