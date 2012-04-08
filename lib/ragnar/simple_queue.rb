a1ad0214b66ec6119e52ae03ecf490dc6f0c5284
Brandon Dewitt <brandonsdewitt@gmail.com>
2012-04-07 18:16:39 -0600

    use class level interface for publishing

--- lib/ragnar/simple_queue.rb
+++ lib/ragnar/simple_queue.rb
@@ -1,33 +1,28 @@
 require 'bunny'
+require 'thread'
+
 module Ragnar
   # Simple publishing via a wrapper around bunny
   class SimpleQueue
-    attr_reader :bunny
 
-    def initialize
-      @bunny = Bunny.new({:host => Ragnar::Config.host,
-                          :port => Ragnar::Config.port,
-                          :logging => false})
+    def self.options
+      @options ||= {
+        :host => Ragnar::Config.host,
+        :port => Ragnar::Config.port,
+        :logging => false
+      }
     end
 
     # Publish to a topic exchange. Defaults to 'events'
     def self.publish_topic(message, route, exchange='events')
-      @sq = Ragnar::SimpleQueue.new
-      @sq.publish_topic(message, route, exchange)
-    end
+      @publish_mutex ||= ::Mutex.new
 
-    def publish_topic(message, route, exchange)
-      ensure_connection
-      exchange = @bunny.exchange(exchange, :type => :topic)
-      exchange.publish(message, :key => route)
-    end
-
-    private
-      def ensure_connection
-        unless(@bunny.connected?)
-          @bunny.stop
-          @bunny.start
+      @publish_mutex.synchronize do 
+        ::Bunny.run(options) do |bunny|
+          exchange = bunny.exchange(exchange, :type => :topic)
+          exchange.publish(message, :key => route)
         end
       end
+    end
   end
 end
